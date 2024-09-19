89f7d3241aae00c8b7e5e4cfaf58e8e5f4fe999c
J0bot <jojo64364@gmail.com>
2024-09-19 12:03:56 +0200

    à ma sauce

---
+++ cmake-build-debug/CMakeFiles/LaboExample1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaboExample1.dir/main.cpp.obj"
+  "CMakeFiles/LaboExample1.dir/main.cpp.obj.d"
+  "LaboExample1.exe"
+  "LaboExample1.exe.manifest"
+  "LaboExample1.pdb"
+  "libLaboExample1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaboExample1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+/*
+Laboratoire : Test git
+Auteur : José Carlos Gasser
+Date : 19.09.2024
+But : exemple de programme
+Remarque(s) : aucune
+ */
+
 #include <iostream>
+#include <cstdlib>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hollo, World!" << std::endl;
     return 0;
 }
